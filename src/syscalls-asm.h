0b3e3d6e690c81352e2f32700044e90b5ead026c
James Yeung <tkoyeung@connect.hku.hk>
2022-06-23 11:51:13 +0800

    Update syscalls-asm.h

--- src/syscalls-asm.h
+++ src/syscalls-asm.h
@@ -98,4 +98,23 @@ syscall \n\
 ret \n\
 ");
 
+#define ZwCreateThreadEx NtCreateThreadEx
+__asm__("NtCreateThreadEx: \n\
+mov [rsp +8], rcx \n\
+mov [rsp+16], rdx \n\
+mov [rsp+24], r8 \n\
+mov [rsp+32], r9 \n\
+sub rsp, 0x28 \n\
+mov ecx, 0x08C53A8EE \n\
+call SW2_GetSyscallNumber \n\
+add rsp, 0x28 \n\
+mov rcx, [rsp +8] \n\
+mov rdx, [rsp+16] \n\
+mov r8, [rsp+24] \n\
+mov r9, [rsp+32] \n\
+mov r10, rcx \n\
+syscall \n\
+ret \n\
+");
+
 #endif
